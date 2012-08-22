65c7e85663aa5bcaebf7c2885ec75c4a67a6feeb
Armen138 <armen138@gmail.com>
2012-08-22 16:48:13 -0400

    improved line drawing

--- js/line-artist.js
+++ js/line-artist.js
@@ -4,7 +4,9 @@
 		data = function() {
 			document.getElementById('data').innerHTML = JSON.stringify(lines);
 		};
-	function drawGrid(ctx) {
+	function drawGrid() {
+		var canvas = document.getElementById('grid'),
+			ctx = canvas.getContext('2d');
 		ctx.lineWidth = 1;
 		ctx.strokeStyle = "black";
 		ctx.beginPath();
@@ -19,28 +21,46 @@
 		ctx.strokeRect(160, 160, 320, 320);
 	}
 
+	function redraw(context) {
+			context.clearRect(0, 0, 640, 640);
+			context.lineWidth = 10;
+			context.strokeStyle = "black";
+			context.beginPath();
+			for(var i = 0; i < lines.length; i++) {
+				context.moveTo(lines[i][0].X * 10 + 320, lines[i][0].Y * 10 + 320);
+				context.lineTo(lines[i][1].X * 10 + 320, lines[i][1].Y * 10 + 320);
+			}
+			context.stroke();
+	}
+
 	window.addEventListener("load", function() {
 		var canvas = document.getElementById('game'),
 			context = canvas.getContext('2d');
-		drawGrid(context);
+		drawGrid();
 
 		canvas.addEventListener("click", function(e) {
 			var x = (e.clientX / 10 - 32) | 0,
 				y = (e.clientY / 10 - 32) | 0;
 			if(current) {
-				context.lineWidth = 10;
-				context.strokeStyle = "black";
-				context.beginPath();
-				context.moveTo(current.X * 10 + 320, current.Y * 10 + 320);
-				context.lineTo(x * 10 + 320, y * 10 + 320);
-				console.log({X: current.X * 10 + 320, Y: current.Y * 10 + 320});
-				console.log({X: x * 10 + 320, Y: y * 10 + 320 });
-				context.stroke();
 				lines.push([current, {X: x, Y:y}]);
 				data();
+				redraw(context);
 				current = null;
 			} else {
 				current = {X: x, Y: y};
+			}
+		});
+		canvas.addEventListener("mousemove", function(e) {
+			var x = (e.clientX / 10 - 32) | 0,
+				y = (e.clientY / 10 - 32) | 0;
+			if(current) {
+				redraw(context);
+				context.lineWidth = 10;
+				context.strokeStyle = "grey";
+				context.beginPath();
+				context.moveTo(current.X * 10 + 320, current.Y * 10 + 320);
+				context.lineTo(x * 10 + 320, y * 10 + 320);
+				context.stroke();
 			}
 		});
 	});
